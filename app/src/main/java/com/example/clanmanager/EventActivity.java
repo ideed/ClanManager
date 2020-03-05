ad64649c28174fc5fbcf2884eb00a3413da990d2
DarraghOBrien692 <xdarraghobrienx@gmail.com>
2020-03-05 12:49:10 +0000

    Darragh third commit

---
+++ app/src/main/java/com/example/clanmanager/EventActivity.java
@@ -0,0 +1,46 @@
+package com.example.clanmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+
+public class EventActivity extends AppCompatActivity {
+
+    private Button back;
+    private Button editEvents;
+    private CalendarView calender;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+
+        back = (Button)findViewById(R.id.button7);
+        editEvents = (Button)findViewById(R.id.button8);
+        calender = (CalendarView)findViewById(R.id.calendarView);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        editEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        calender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/clanmanager/MemberActivity.java
@@ -0,0 +1,191 @@
+package com.example.clanmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.SearchView;
+
+public class MemberActivity extends AppCompatActivity {
+
+    private Button back;
+    private Button addMember;
+    private Button member1;
+    private Button member2;
+    private Button member3;
+    private Button member4;
+    private Button member5;
+    private Button member6;
+    private Button member7;
+    private Button member8;
+    private Button member9;
+    private Button member10;
+    private Button member11;
+    private Button member12;
+    private Button member13;
+    private Button member14;
+    private Button member15;
+    private SearchView search;
+    private ScrollView scroll;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member);
+
+        back = (Button)findViewById(R.id.button1);
+        addMember = (Button)findViewById(R.id.button2);
+        member1 = (Button)findViewById(R.id.button3);
+        member2 = (Button)findViewById(R.id.button4);
+        member3 = (Button)findViewById(R.id.button5);
+        member4 = (Button)findViewById(R.id.button6);
+        member5 = (Button)findViewById(R.id.button7);
+        member6 = (Button)findViewById(R.id.button8);
+        member7 = (Button)findViewById(R.id.button9);
+        member8 = (Button)findViewById(R.id.button10);
+        member9 = (Button)findViewById(R.id.button11);
+        member10 = (Button)findViewById(R.id.button12);
+        member11 = (Button)findViewById(R.id.button13);
+        member12 = (Button)findViewById(R.id.button14);
+        member13 = (Button)findViewById(R.id.button15);
+        member14 = (Button)findViewById(R.id.button16);
+        member15 = (Button)findViewById(R.id.button17);
+        search = (SearchView)findViewById(R.id.searchView1);
+        scroll = (ScrollView)findViewById(R.id.scrollView1);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        addMember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        member15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        scroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
